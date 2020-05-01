c74ebe04a260b1dcba4d3af36af778a7fb1931fc
Lily <lilyhuan@andrew.cmu.edu>
2020-05-01 14:58:35 -0500

    pass ability tests

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,49 +28,70 @@ class Ability
   
         # they can show assignments belonging to their store
         can :show, Assignment do |a|
-          my_stores = user.stores.map(&:id)  
-          my_assignments = Assignment.select { |ass| my_stores.include? (ass.store_id) }
-          my_assignments.include? a.id
+          my_stores = user.stores  
+        #   my_assignments = Assignment.select { |ass| my_stores.include? (ass.store_id) }
+        #   my_assignments.include? a.id
+          my_assignments = Array.new
+          my_stores.each { |my_store|
+            my_assignments << my_store.assignments
+          }
+          my_assignments = my_assignments.flatten
+          my_assignments.include? a
         end
 
         can :show, Employee do |e|
-          my_employees = Employee.select { |emp| my_stores.include? (emp.store.id) }
-          my_employees.include? e.id
+        #   my_employees = Employee.select { |emp| my_stores.include? (emp.store.id) }
+        #   my_employees.include? e.id
+            my_stores = user.stores
+            my_employees = Array.new
+            my_stores.each { |my_store|
+                my_employees << my_store.employees
+            }
+            my_employees = my_employees.flatten
+            my_employees.include? e
         end
 
         can :edit, Employee do |e|
-          my_employees = Employee.select { |emp| my_stores.include? (emp.store.id) }
-          my_employees.include? e.id
+            my_stores = user.stores
+            my_employees = Array.new
+            my_stores.each { |my_store|
+                my_employees << my_store.employees
+            }
+            my_employees = my_employees.flatten
+            my_employees.include? e
         end
 
         can :update, Employee do |e|
-          my_employees = Employee.select { |emp| my_stores.include? (emp.store.id) }
-          my_employees.include? e.id
+            my_stores = user.stores
+            my_employees = Array.new
+            my_stores.each { |my_store|
+                my_employees << my_store.employees
+            }
+            my_employees = my_employees.flatten
+            my_employees.include? e
         end
   
   
       elsif user.role? :employee
-        # they can read their own data
-        can :show, Owner do |this_owner|  
-          user.owner == this_owner
+        can :index, Assignment
+
+        # they can read their own assignment details
+        can :show, Assignment do |a|  
+          user.assignments.include? a
         end
   
-        # they can see lists of pets and visits (controller filters automatically)
-        can :index, Pet
-        can :index, Visit
-  
-        # they can read their own pets' data
-        can :show, Pet do |this_pet|  
-          my_pets = user.owner.pets.map(&:id)
-          my_pets.include? this_pet.id 
+        # they can read their own employee data
+        can :show, Employee do |e|  
+          user.id == e.id
+        end
+
+        can :edit, Employee do |e|  
+            user.id == e.id
         end
   
-        # they can read their own visits' data
-        can :show, Visit do |this_visit|  
-          my_visits = user.owner.visits.map(&:id)
-          my_visits.include? this_visit.id 
+        can :update, Employee do |e|  
+            user.id == e.id
         end
-  
         
       else
         # guests can only read animals covered (plus home pages)
